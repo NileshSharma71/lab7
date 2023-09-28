e91933c4342360c759953f8af0af692bf6be6dfc
Nilesh Sharma <sharmanilesh7105@gmail.com>
2023-09-28 14:38:18 +0530

    Added some files

---
+++ question9.py
@@ -0,0 +1,14 @@
+n=input("Enter: ")
+k=[]
+count=0
+for i in range(65,91):
+    op=chr(i)
+    k.append(op)
+
+for kj in k:
+    if kj in n:
+        count+=1
+if count==26:
+    print("The string is pangram.")
+else:
+    print("The string is not pangram.")
